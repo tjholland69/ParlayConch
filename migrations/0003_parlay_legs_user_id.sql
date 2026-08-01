5bab865a8d81113373a14bb4b2ce9e5734b91ee0
tholland69 <holltj12@alumni.wfu.edu>
2026-08-01 18:12:28 -0400

    updated my integration tests ts file

---
+++ migrations/0003_parlay_legs_user_id.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "parlay_legs" ADD COLUMN IF NOT EXISTS "user_id" varchar;--> statement-breakpoint
+ALTER TABLE "parlay_legs" ADD CONSTRAINT "parlay_legs_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+CREATE INDEX "parlay_legs_user_id_idx" ON "parlay_legs" USING btree ("user_id");
